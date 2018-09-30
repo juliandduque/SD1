80957810f1c6f67c40b51e94f081fd33887d248a
juliandduque <juliandduque@hotmail.com>
2018-09-30 18:34:14 -0400

    test

--- web/API/API.php
+++ web/API/API.php
@@ -74,7 +74,8 @@ function createRecord($dbConnection, $jsonPayload)
   // This block uses prepared statements and parameterized queries to protect against SQL injection
         // MySQL query to add the username and password into the database
         $query = $dbConnection->prepare("INSERT INTO data (datetime, Frequency, deviceID, key) VALUES ('?', '?', '?', '?')");
-        $query->bind_param('siss', $datetime, $frequency, $deviceID, $datetime.$frequency.$deviceID);
+		$key = $datetime.$frequency.$deviceID;
+        $query->bind_param('siss', $datetime, $frequency, $deviceID, $key);
         $query->execute();
 		
         // Result from the query
