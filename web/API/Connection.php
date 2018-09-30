a6790623ca2e42f9541e23dad2cba744cf7aa210
juliandduque <juliandduque@hotmail.com>
2018-09-30 17:40:49 -0400

    connection

--- web/API/Connection.php
+++ web/API/Connection.php
@@ -13,7 +13,7 @@ function establishConnection()
         // Try to establish a connection
         // The @ suppresses warning messages so they don't print on the page
 		file_put_contents("php://stderr", $secrets['host'] . " " . $secrets['username']. " " . $secrets['passwd']. " " . $secrets['dbname']);
-        $dbConnection = @new mysqli($secrets['host'], $secrets['username'], $secrets['passwd'], $secrets['dbname']);
+        $dbConnection = @new mysqli($secrets['host'], $secrets['username'], $secrets['passwd'], $secrets['dbname'], 3306);
 		file_put_contents("php://stderr", "connection stablished");
     } catch (Exception $e) {
         // If there is an exception (not warning or error), return exception message as JSON string
