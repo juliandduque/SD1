e74b30251bb244d023bfa82e34cb162a12412da1
Julian Duque <juliandduque@hotmail.com>
2018-11-19 19:03:05 -0500

    test

--- web/API/API.php
+++ web/API/API.php
@@ -77,23 +77,23 @@ function createRecord($dbConnection, $jsonPayload)
 			// This block uses prepared statements and parameterized queries to protect against SQL injection
 			// MySQL query to add the username and password into the database
 			$query = $dbConnection->prepare("INSERT INTO `chatterboxDB`.`data` (`datetime`, `Frequency`, `deviceID`, `strength`) VALUES ('".$time."', '".$frequency."', '".$deviceID."', '".$strength."')");
+			$query->execute();    
+		
+			 // Result from the query
+			$result = $query->get_result();
 
+			// Check to see if the insertion was successful...
+			if ($result) {
+				// If successful, return JSON success response
+				returnSuccess('Record created.');
+			} else {
+				// If not successful, return JSON error response
+				returnError($dbConnection->error);
+			}
 		}
 	}
 
-	$query->execute();    
-		
-	 // Result from the query
-	$result = $query->get_result();
-
-	// Check to see if the insertion was successful...
-	if ($result) {
-		// If successful, return JSON success response
-		returnSuccess('Record created.');
-	} else {
-		// If not successful, return JSON error response
-		returnError($dbConnection->error);
-	}
+	
 }
 
 /**
